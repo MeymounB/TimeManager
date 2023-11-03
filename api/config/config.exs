a5e89cecf57d5c3afd135a7df43a5fab5af38841
Andreas Leroux <andreas.leroux@epitech.eu>
2023-11-03 15:26:02 +0100

    Set secret key base as environnment variable

--- api/config/config.exs
+++ api/config/config.exs
@@ -24,7 +24,7 @@ config :phoenix_swagger, json_library: Jason
 # Configures the endpoint
 config :time_manager, TimeManagerWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: "8ZxsxXy6roDD8KWSgF1J+WbwKDKx4WMgWHcxp2u7bvsOGTOsPqnHEVCxddEcuO6v",
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   adapter: Phoenix.Endpoint.Cowboy2Adapter,
   render_errors: [
     formats: [json: TimeManagerWeb.ErrorJSON],
