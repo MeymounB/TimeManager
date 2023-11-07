0e6bdbc185f97a264636550dc33d62fc25ea7cdc
Meymoun Boualaoui <boualaoui.meymoun.icn@gmail.com>
2023-11-07 01:00:09 +0100

    add App Footer

---
+++ front/components/AppFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer class="footer-section bg-gray-800 text-white py-8">
+    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex flex-wrap justify-center md:justify-between items-center">
+        <nav class="w-full md:w-auto mb-4 md:mb-0">
+          <ul class="flex flex-wrap justify-center space-x-4">
+            <li><a href="/contact" class="hover:text-gray-300">Contact</a></li>
+            <li><a href="/faq" class="hover:text-gray-300">FAQ</a></li>
+            <li><a href="/terms" class="hover:text-gray-300">Termes et Conditions</a></li>
+          </ul>
+        </nav>
+        <div class="flex space-x-4">
+          <a href="https://facebook.com" target="_blank" class="hover:text-gray-300">
+            <!--  icone à la place de FB -->
+            FB
+          </a>
+          <a href="https://twitter.com" target="_blank" class="hover:text-gray-300">
+            <!--  icone à la place de TW -->
+            TW
+          </a>
+          <a href="https://instagram.com" target="_blank" class="hover:text-gray-300">
+            <!--  icone à la place de IG -->
+            IG
+          </a>
+        </div>
+      </div>
+      <p class="text-center text-gray-500 text-sm mt-8">
+        &copy; 2023 Time Manager. Tous droits réservés.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+/* Styles for the footer section */
+</style>
