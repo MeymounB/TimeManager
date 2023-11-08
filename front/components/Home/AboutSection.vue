147bf2b53bb24cbb5ac8eaddc2e4d51f64a0753b
Meymoun Boualaoui <boualaoui.meymoun.icn@gmail.com>
2023-11-08 10:26:55 +0100

    add svg (landing page)

--- front/components/Home/AboutSection.vue
+++ front/components/Home/AboutSection.vue
@@ -12,7 +12,7 @@
     <div class="features-list grid grid-cols-1 gap-6 md:grid-cols-3">
       <div class="feature-item bg-white p-6 shadow-sm rounded-lg flex flex-col items-center text-center">
       <!-- Image -->
-        <svg-icon class="h-12 w-12 mb-3" src="/" alt="Gestion du Temps" name="cross" />
+        <svg-icon class="h-12 w-12 mb-3" src="/" name="clock" />
         <h3 class="mb-2 font-semibold text-gray-900">Gestion du Temps</h3>
         <p class="text-sm text-gray-600">
           Suivi précis des heures de travail pour optimiser la productivité.
@@ -21,7 +21,7 @@
 
       <div class="feature-item bg-white p-6 shadow-sm rounded-lg flex flex-col items-center text-center">
       <!-- Image -->
-        <svg-icon class="h-12 w-12 mb-3"  alt="Gestion d'Équipe" name="cross" />
+        <svg-icon class="h-12 w-12 mb-3"  alt="Gestion d'Équipe" name="team" />
         <h3 class="mb-2 font-semibold text-gray-900">Gestion d'Équipe</h3>
         <p class="text-sm text-gray-600">
           Organisation des équipes avec des outils de communication intégrés.
@@ -30,7 +30,7 @@
 
       <div class="feature-item bg-white p-6 shadow-sm rounded-lg flex flex-col items-center text-center">
       <!-- Image -->
-        <svg-icon class="h-12 w-12 mb-3" alt="Rapports Détaillés" name="cross" />
+        <svg-icon class="h-12 w-12 mb-3" alt="Rapports Détaillés" name="clipboard" />
         <h3 class="mb-2 font-semibold text-gray-900">Rapports Détaillés</h3>
         <p class="text-sm text-gray-600">
           Des rapports d'activités complets pour un suivi en temps réel.
--- front/components/Home/FeaturesDetailSection.vue
+++ front/components/Home/FeaturesDetailSection.vue
@@ -6,7 +6,7 @@
       </h2>
        <div class="grid grid-cols-1 gap-6 sm:gap-8 sm:grid-cols-2 lg:grid-cols-3 items-stretch">
         <div class="feature-item flex flex-col sm:flex-row">
-          <svg-icon class="h-24 w-24 sm:h-32 sm:w-32 flex-shrink-0 mb-4 sm:mb-0 sm:mr-4" name="cross" alt="Dashboard" />
+          <svg-icon class="h-24 w-24 sm:h-32 sm:w-32 flex-shrink-0 mb-4 sm:mb-0 sm:mr-4" name="presentation-chart" />
           <div class="flex flex-col justify-center">
             <h3 class="font-semibold text-lg text-gray-900">Tableaux de Bord</h3>
             <p class="text-sm sm:text-base text-gray-600">
@@ -16,7 +16,7 @@
         </div>
 
          <div class="feature-item flex flex-col sm:flex-row">
-          <svg-icon class="h-24 w-24 sm:h-32 sm:w-32 flex-shrink-0 mb-4 sm:mb-0 sm:mr-4" name="cross" alt="Dashboard" />
+          <svg-icon class="h-24 w-24 sm:h-32 sm:w-32 flex-shrink-0 mb-4 sm:mb-0 sm:mr-4" name="team" />
           <div class="flex flex-col justify-center">
             <h3 class="font-semibold text-lg text-gray-900">Gestion des Équipes</h3>
             <p class="text-sm sm:text-base text-gray-600">
@@ -26,7 +26,7 @@
         </div>
 
          <div class="feature-item flex flex-col sm:flex-row">
-          <svg-icon class="h-24 w-24 sm:h-32 sm:w-32 flex-shrink-0 mb-4 sm:mb-0 sm:mr-4" name="cross" alt="Dashboard" />
+          <svg-icon class="h-24 w-24 sm:h-32 sm:w-32 flex-shrink-0 mb-4 sm:mb-0 sm:mr-4" name="document-chart" />
           <div class="flex flex-col justify-center">
             <h3 class="font-semibold text-lg text-gray-900">Rapports d'Heures</h3>
             <p class="text-sm sm:text-base text-gray-600">
